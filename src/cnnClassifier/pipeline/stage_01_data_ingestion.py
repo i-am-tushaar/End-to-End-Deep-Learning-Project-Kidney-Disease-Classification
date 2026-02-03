98527726b030b7351ab8880188f7c5a92a1cdb85
Tushaar <tushaar@example.com>
2026-02-03 18:18:14 +0530

    Integrated DVC for data versioning and model tracking

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,6 +1,7 @@
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
-from cnnClassifier import logger
+from src.cnnClassifier import logger
+
 
 STAGE_NAME = "Data Ingestion stage"
 
